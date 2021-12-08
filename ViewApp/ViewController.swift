39658b43d1b8837b0b8dd750187cc00f7985166b
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-08 11:06:05 +0300

    Initial Commit

---
+++ ViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewApp
+//
+//  Created by Диана Овечкина on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
